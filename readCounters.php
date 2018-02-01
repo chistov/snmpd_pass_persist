55b90c3cdaf6e6f69ab6b165921622ea7c8d2b7a
chistov <msn_mailbox@mail.ru>
2018-02-01 12:14:00 +0300

    Update readCounters.php

--- readCounters.php
+++ readCounters.php
@@ -1,31 +1,17 @@
 #!/usr/bin/php
 <?php
-/*****************************************************
- * Package ZypioSNMP
- * Description ZypioSNMP is used to datafill SNMP OID's
- *             using SNMP's pass functionality
- * Author Mike Mackintosh < m@zyp.io >
- * Date 20130703
- * Version 1.1
- *
+ /*Package originally developed by Mike Mackintosh
+ * Fixed all bugs with pass_persist and performance by Sergey Chistov < msn_mailbox@mail.ru >
  * Requires >= PHP5.4
  *
  ****************************************************/
-
-require_once( __DIR__ . "/ZypioSNMP.php");
 require_once( __DIR__ . "/IO.php");
-
-
-
-// DEBUG("stdin: ". DEBUG_VAR($_SERVER));
-// Log Request
-//file_put_contents("/tmp/zypiosnmp.log", "Conneciton Received\n", FILE_APPEND);
-
+require_once( __DIR__ . "/ZypioSNMP.php");
 
 // Instantiate Class using Base OID
 // -- this base OID must match what you  
 // added the pass statement in snmpd.conf
-$snmp = new ZypioSNMP(".1.3.6.1.4.1.42779.3");
+$snmp = new ZypioSNMP(".1.3.6.1.4.1.YourNumber");
 
 // The below syntax adds to the base .1.3.6.1.4.1.38741
 ///*
@@ -33,7 +19,7 @@ $snmp = new ZypioSNMP(".1.3.6.1.4.1.42779.3");
 
 // DEBUG("counters: ". DEBUG_VAR($counters));
 
-$snmp->addOid(".1", ZypioSNMP::STRING, "NanoSwitch 3"); // .1.3.6.1.4.1.38741.1.0
+$snmp->addOid(".1", ZypioSNMP::STRING, "My HW Name"); // .1.3.6.1.4.1.38741.1.0
 
 for ($i = 0; $i < 64; ++$i){
   $snmp->addOid(".2." . $i, ZypioSNMP::COUNTER, 0);//$counters['link'][$i]);
